ed0f191f63273134888da80e8c1fa5edd3d8b7a2
Remave <anton.init404@gmail.com>
2022-07-06 22:51:33 +0300

    shell

--- shell/shell.c
+++ shell/shell.c
@@ -1,12 +1,13 @@
 
 #include "shell.h"
 
-/*
- _____             _           _____ _____ 
-|  _  |___ ___ ___| |_ ___ _ _|     |   __|
-|   __| .'|  _| .'| . | . |_'_|  |  |__   |
-|__|  |__,|_| |__,|___|___|_,_|_____|_____|
-*/
+#define PRINT_ASCIIART \
+	kprint("  _____             _           _____ _____ \n"); \
+	kprint(" |  _  |___ ___ ___| |_ ___ _ _|     |   __|\n"); \
+	kprint(" |   __| .'|  _| .'| . | . |_'_|  |  |__   |\n"); \
+	kprint(" |__|  |__,|_| |__,|___|___|_,_|_____|_____|\n")
+
+
 
 static const int8_t SHELL_VERSION_MAJOR = 1;
 static const int8_t SHELL_VERSION_MINOR = 1;
@@ -21,10 +22,7 @@ void load_shell()
 	// state_shell.status = NULL; //NOPING
 	INIT_VGA_COLOR;
 	kprint("Wellcom to ParaboxOS v1.0 [Shell v1.1]\n");
-	kprint("  _____             _           _____ _____ \n");
-	kprint(" |  _  |___ ___ ___| |_ ___ _ _|     |   __|\n");
-	kprint(" |   __| .'|  _| .'| . | . |_'_|  |  |__   |\n");
-	kprint(" |__|  |__,|_| |__,|___|___|_,_|_____|_____|\n");
+	PRINT_ASCIIART;
 	kprint("\n");
 	kprint("type help for commands list, <command> -h for info a command.\n");
 	set_color(BG_BLACK, FG_LIGHT_RED);
